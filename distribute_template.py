ec989452d5299eaa7fa643fcf740b1aff57e6300
MrHallKellett <chall@kellettschool.com>
2024-12-15 19:17:19 +0800

    fix template distro

--- distribute_template.py
+++ distribute_template.py
@@ -6,7 +6,7 @@ day = input("Enter day :")
 
 with open(f"day{day}a.py", "w") as f:
      f.write(template.replace("DAY = 1", f"DAY = {day}"))
-with open(f"day{5}.txt", "w") as f:
+with open(f"day{day}.txt", "w") as f:
      f.write()
 
 print("Prepared template")
